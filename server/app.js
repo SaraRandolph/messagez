4fa9156b28e24a1ee767194f2d0fb915de058198
Sara Randolph <sara.randolph222@gmail.com>
2018-06-24 14:03:57 -0600

    trying things again

--- server/app.js
+++ server/app.js
@@ -7,14 +7,13 @@ var setupController = require('./controllers/setupController');
 var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3005;
-// var port = process.env.PORT;
 
 app.use('/', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
-setupController(app);
+// setupController(app);
 apiController(app);
 
 app.listen(port);
